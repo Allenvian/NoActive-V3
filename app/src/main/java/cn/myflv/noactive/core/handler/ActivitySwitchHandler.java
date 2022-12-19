613195b19014579ef9400574c3d99fb12042b93a
Xin <admin@myflv.cn>
2022-12-19 14:14:14 +0800

    优化

--- app/src/main/java/cn/myflv/noactive/core/handler/ActivitySwitchHandler.java
+++ app/src/main/java/cn/myflv/noactive/core/handler/ActivitySwitchHandler.java
@@ -11,6 +11,9 @@ public class ActivitySwitchHandler {
     public static AppInfo lastApp = AppInfo.getInstance(CommonConstants.ANDROID);
 
     public static void newEvent(AppInfo currentApp) {
+        if (lastApp.getKey().equals(currentApp.getKey())) {
+            return;
+        }
         XLog.d("当前应用", currentApp.getKey());
         // 解冻当前App
         FreezerHandler.unFreezeIfNeed(currentApp);
