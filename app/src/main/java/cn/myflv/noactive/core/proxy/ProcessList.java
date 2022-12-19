b9f12f651e97147ab79df81fefb4a597aa3217e7
Xin <admin@myflv.cn>
2022-12-19 14:26:17 +0800

    优化

---
+++ app/src/main/java/cn/myflv/noactive/core/proxy/ProcessList.java
@@ -0,0 +1,53 @@
+package cn.myflv.noactive.core.proxy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cn.myflv.noactive.constant.ClassConstants;
+import cn.myflv.noactive.constant.FieldConstants;
+import cn.myflv.noactive.constant.MethodConstants;
+import cn.myflv.noactive.core.entity.ProcessRecord;
+import de.robv.android.xposed.XposedHelpers;
+import lombok.Data;
+
+@Data
+public class ProcessList {
+    private final Object processList;
+
+    public ProcessList(Object processList) {
+        this.processList = processList;
+    }
+
+    public static void setOomAdj(ClassLoader classLoader, int pid, int uid, int oomAdj) {
+        Class<?> ProcessList = XposedHelpers.findClass(ClassConstants.ProcessList, classLoader);
+        XposedHelpers.callStaticMethod(ProcessList, MethodConstants.setOomAdj, pid, uid, oomAdj);
+    }
+
+    public Map<String, List<ProcessRecord>> getProcessMap() {
+        Map<String, List<ProcessRecord>> processMap = new HashMap<>();
+        synchronized (processList) {
+            try {
+                List<?> processRecordList = (List<?>) XposedHelpers.getObjectField(processList, FieldConstants.mLruProcesses);
+                for (Object proc : processRecordList) {
+                    ProcessRecord processRecord = new ProcessRecord(proc);
+                    String packageName = processRecord.getPackageName();
+                    List<ProcessRecord> list = processMap.computeIfAbsent(packageName, k -> new ArrayList<>());
+                    list.add(processRecord);
+                }
+            } catch (Exception ignored) {
+
+            }
+        }
+        return processMap;
+    }
+
+    public List<ProcessRecord> getProcessList(String packageName) {
+        List<ProcessRecord> processRecords = getProcessMap().get(packageName);
+        if (processRecords == null) {
+            return new ArrayList<>();
+        }
+        return processRecords;
+    }
+}
